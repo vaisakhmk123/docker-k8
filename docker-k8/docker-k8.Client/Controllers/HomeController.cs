cd99296e21f873a58ca97cd54290289d8e8f2f20
vaisakhmk123@gmail.com <VMK@github2024>
2024-03-19 14:13:38 +0000

    Initial mvc app commit

---
+++ docker-k8/docker-k8.Client/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using docker_k8.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace docker_k8.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
